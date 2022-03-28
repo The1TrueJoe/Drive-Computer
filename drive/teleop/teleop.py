f5afcd0cf70c8ee92ed9bb3a9285ec4ddefa4076
The1TrueJoe <jttelaak@hotmail.com>
2022-03-28 14:50:19 -0400

    Function mapper for teleop operations

--- drive/teleop/teleop.py
+++ drive/teleop/teleop.py
@@ -45,6 +45,39 @@ class Teleop:
         # Connector
         self.connector = Remote_Drive_Server(gamepad=self.gamepad)
 
+        # Controller Buttons
+        self.button_map = {
+            'BTN_TL': self.noAction,
+            'BTN_TR': self.noAction,
+            'BTN_NORTH': self.noAction,
+            'BTN_EAST': self.noAction,
+            'BTN_SOUTH': self.noAction,
+            'BTN_WEST': self.noAction,
+            'BTN_THUMBL': self.noAction,
+            'BTN_THUMBR': self.noAction,
+            'BTN_START': self.noAction,
+            'BTN_SELECT': self.noAction,
+            'DPAD_NORTH': self.noAction,
+            'DPAD_SOUTH': self.noAction,
+            'DPAD_EAST': self.noAction,
+            'DPAD_WEST': self.noAction
+
+        }
+
+        self.stick_map = {
+            'LSTICK_X': self.noAction,
+            'LSTICK_Y': self.noAction,
+            'RSTICK_X': self.noAction,
+            'RSTICK_Y': self.noAction
+
+        }
+
+        self.trigger_map = {
+            'ABS_Z': self.noAction,
+            'ABS_RZ': self.noAction
+
+        }
+
     # Mode Initialization, Called Once
     def initialize(self):
         self.my_cart.applyTeleop()
@@ -65,7 +98,20 @@ class Teleop:
     def perodic(self):
         ### NOTE: Connections Are Killed From the Client With a Pre-Defined Button
         while self.connector.connection_established and not self.kill:
-            pass
+            for button_event in self.gamepad.buttons.keys():
+                if self.gamepad.buttons[button_event]:
+                    self.button_map[button_event]()
+                    continue
+
+            for stick_event in self.gamepad.sticks.keys():
+                if self.gamepad.sticks[stick_event] != 0.0:
+                    self.stick_map[stick_event](self.gamepad.sticks[stick_event])
+                    continue
+
+            for trigger_event in self.gamepad.triggers.keys():
+                if self.gamepad.triggers[trigger_event] != 0:
+                    self.trigger_map[trigger_event](self.gamepad.triggers[trigger_event])
+                    continue
 
     # Kills the Periodic Loop NOTE: Closing process is in the exit method
     def kill(self):
@@ -74,3 +120,6 @@ class Teleop:
     def exit(self):
         self.connector.kill()
         self.my_cart.completeStop()
+
+    def noAction(self):
+        pass
